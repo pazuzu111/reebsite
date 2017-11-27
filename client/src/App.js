27f82156442d1fa5b76ba73dce3f24ad6d32d3d8
abrian rodriguez <pazuzublue111@gmail.com>
2017-11-27 15:52:53 -0500

    setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import BeerController from './components/BeerController';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/beer"
+              render={() => <BeerController currentPage="index" />}
+            />
+            <Route exact path="/new" render={() => (<BeerController currentPage="new" />)} />
+            <Route exact path="/beer/edit/:id"
+              render={props => (<BeerController
+                currentPage="edit" currentId={props.match.params.id} />)}
+            />
+            <Route exact path="/beer/:id"
+              render={props => (<BeerController
+                currentPage="show" currentId={props.match.params.id} />)}
+            />
+          </div>
+          <Footer />
+        </div>
+      </Router>
 
       </div>
     );
