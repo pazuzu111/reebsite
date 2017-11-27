2bcddccd3158eb435c21fbe3beed2e76a60fe6b5
abrian rodriguez <pazuzublue111@gmail.com>
2017-11-27 16:18:43 -0500

    home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Beer App</h1>
+      <h3>where's the game?</h3>
+    </div>
+  );
+};
+
+export default Home;
