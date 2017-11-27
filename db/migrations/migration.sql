47f85475658a97670e81ee0d0e52c599e350de65
Syed Shah <syed.shah@nyu.edu>
2017-11-27 14:18:04 -0500

    Create /db with migrations and seeds, create config.js

---
+++ db/migrations/migration.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF not exists beer (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  brewery VARCHAR(255),
+  country VARCHAR(255),
+  abv VARCHAR(255)
+);
---
+++ db/seeds/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO beer (name, brewery, country, abv) VALUES ('Abrian Special','Abrian Brewery House', 'Egypt', '8%');
+INSERT INTO beer (name, brewery, country, abv) VALUES ('Raging Bitch','Flying Dog', 'USA', '8.3%');
+INSERT INTO beer (name, brewery, country, abv) VALUES ('Snakedog','Flying Dog', 'USA', '8.3%');
+INSERT INTO beer (name, brewery, country, abv) VALUES ('Morning Delight','Toppling', 'USA', '12%');
+INSERT INTO beer (name, brewery, country, abv) VALUES ('Julius','Treehouse', 'USA', '6.8%');
