bcf0f104518b32ba23683376d4b2c59459576db1
Syed Shah <syed.shah@nyu.edu>
2017-11-28 14:01:32 -0500

    Just a quick commit to update minor change

--- client/src/Beerstyle.js
+++ client/src/Beerstyle.js
@@ -1,20 +1,20 @@
 import React from 'react';
-
+import './App.css';
 
 const Beerstyle = props => {
   console.log("Beerstyle")
   console.log(props)
     return(
-        props.dataLoaded ?
+      <div className="scrollContainer">
+        {props.dataLoaded ?
             props.styleList.map((x, i) => {
             return (
-              <div className="scrollContainer">
-                <h1 key={i}  onClick={props.beerOnClick(props.styleList[i].id)}> {props.styleList[i].name} </h1>)
-              </div>
-            })
+              <h1 key={i}  onClick={props.beerOnClick(props.styleList[i].id)}> {props.styleList[i].name} </h1>
+            )})
             :
-            <p> Loading... </p>
-      )
+            <p> Loading... </p>}
+      </div>
+    )
 }
 
 export default Beerstyle
