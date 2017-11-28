3e26ad34c596fee62f79ec21f49d5525f31b4a20
Syed Shah <syed.shah@nyu.edu>
2017-11-28 13:43:00 -0500

    Test

--- client/src/Beerstyle.js
+++ client/src/Beerstyle.js
@@ -7,7 +7,10 @@ const Beerstyle = props => {
     return(
         props.dataLoaded ?
             props.styleList.map((x, i) => {
-            return (<h1 key={i}  onClick={props.beerOnClick(props.styleList[i].id)}> {props.styleList[i].name} </h1>)
+            return (
+              <div className="scrollContainer">
+                <h1 key={i}  onClick={props.beerOnClick(props.styleList[i].id)}> {props.styleList[i].name} </h1>)
+              </div>
             })
             :
             <p> Loading... </p>
